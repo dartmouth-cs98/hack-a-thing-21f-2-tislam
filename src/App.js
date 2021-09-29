a88edf10c7ebea9aaa63ae4e5fc33bc876ebacd6
TanvirI <Islam.Tanvir@hotmail.com>
2021-09-29 02:54:30 -0400

    Added textbox functionallity

--- src/App.js
+++ src/App.js
@@ -2,27 +2,16 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Notes from './pages/Notes'
 import Create from './pages/Create'
 import React from 'react'
-import { TextField } from '@material-ui/core'
 
 import { createMuiTheme, ThemeProvider } from '@material-ui/core'
+import { green, purple } from '@material-ui/core/colors'
 
 
  const theme = createMuiTheme({
-  root: {
-    background: 'linear-gradient( 45deg, #333, #999)',
-    border: 0, 
-    color: 'black',
-    padding: '0 30px'
-    
-  }, 
+ 
   palette: {
-    type: "dark",
-    primary: {
-      main: "#26a27b"
-    },
-    secondary: {
-      main: "#fafafa"
-    }
+    primary: green,
+    secondary: purple
    }
 
  })
